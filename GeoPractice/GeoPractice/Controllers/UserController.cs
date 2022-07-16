25cd96dab838929c1581c71452c484e1e887f0e3
Xch4rt <pab203.guti@gmail.com>
2022-07-15 23:43:51 -0600

    First commit, controllers en proceso

---
+++ GeoPractice/GeoPractice/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser()
+        { 
+            return Ok();
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult UpdateUser()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteUser")]
+        public IActionResult DeleteUser()
+        { 
+            return Ok();
+        }
+    }
+}
